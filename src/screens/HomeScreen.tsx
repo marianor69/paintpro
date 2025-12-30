88d0e1a79e0f54cf84644bc6608bbfa39a41cdc9
john <mariano.rodriguez@gmail.com>
2025-12-30 09:53:00 -0500

    Update version to 9950198

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -90,7 +90,7 @@ export default function HomeScreen({ navigation }: Props) {
               New Interface
             </Text>
             <Text style={{ fontSize: 18, fontWeight: "600", color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center", fontFamily: Platform.OS === "ios" ? "Menlo" : "monospace" }}>
-              CF-001 (aef0a03) - Dec 30, 2024
+              CF-001v2 (9950198) - Dec 30, 2024
             </Text>
             <Text style={{ ...Typography.body, color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center" }}>
               Fast, Accurate, Professional Paint Estimates
