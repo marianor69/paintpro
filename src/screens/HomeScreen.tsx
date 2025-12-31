1c2fcdb84f40aa6df81dceb79d494cdd2f1daeb3
john <mariano.rodriguez@gmail.com>
2025-12-30 19:20:49 -0500

    Update version to UI-001 (dc2b1a8)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -90,7 +90,7 @@ export default function HomeScreen({ navigation }: Props) {
               New Interface
             </Text>
             <Text style={{ fontSize: 18, fontWeight: "600", color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center", fontFamily: Platform.OS === "ios" ? "Menlo" : "monospace" }}>
-              UI-001 (c3a4c40) - Dec 30, 2024
+              UI-001 (dc2b1a8) - Dec 30, 2024
             </Text>
             <Text style={{ ...Typography.body, color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center" }}>
               Fast, Accurate, Professional Paint Estimates
