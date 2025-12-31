813cc12fb68d7fc18a08dd60b2aedd1a9209a7b4
john <mariano.rodriguez@gmail.com>
2025-12-31 15:16:24 -0500

    Update version to bd1dca8

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -90,7 +90,7 @@ export default function HomeScreen({ navigation }: Props) {
               New Interface
             </Text>
             <Text style={{ fontSize: 18, fontWeight: "600", color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center", fontFamily: Platform.OS === "ios" ? "Menlo" : "monospace" }}>
-              SUMM-013 (8f4681f) - Dec 31, 2025
+              SUMM-014 (bd1dca8) - Dec 31, 2025
             </Text>
             <Text style={{ ...Typography.body, color: Colors.mediumGray, marginTop: Spacing.xs, textAlign: "center" }}>
               Fast, Accurate, Professional Paint Estimates
