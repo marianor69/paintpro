7dabe52304a724ede5917febc8323c23e65d463a
john <mariano.rodriguez@gmail.com>
2026-01-18 22:30:13 -0500

    Tweak estimate button label

--- src/screens/ProjectDetailScreen.tsx
+++ src/screens/ProjectDetailScreen.tsx
@@ -863,7 +863,7 @@ export default function ProjectDetailScreen({ route, navigation }: Props) {
                   >
                     <View style={{ alignItems: "center" }}>
                       <Text style={{ fontSize: Typography.caption.fontSize, fontWeight: "600" as any, color: Colors.white, textAlign: "center" }}>
-                        {project.estimateBuildComplete ? "Estimate Completed" : "Complete Estimate."}
+                        {project.estimateBuildComplete ? "Estimate Completed" : "Complete Estimate"}
                       </Text>
                     </View>
                   </Pressable>
